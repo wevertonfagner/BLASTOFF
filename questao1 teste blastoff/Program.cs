bd6f4e8e28ab94535a7853d6e87aa2308897e748
weverton fagner <wevertoonfagneeer@mail.com>
2022-02-16 13:01:09 -0300

    commit para teste

---
+++ questao1 teste blastoff/Program.cs
@@ -0,0 +1,22 @@
+﻿int idadeI, idadeJ, idadeK, idadeX, idadeY;
+int mediaIdades;
+
+Console.WriteLine("Digite idade I: ");
+idadeI = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite idade J: ");
+idadeJ = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite idade K: ");
+idadeK = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite idade X: ");
+idadeX = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite idade Y: ");
+idadeY = int.Parse(Console.ReadLine());
+
+mediaIdades = (idadeI + idadeJ + idadeK + idadeX + idadeY) / 5;
+Console.WriteLine("A média de idade é: " + mediaIdades);
+
+Console.ReadKey();
